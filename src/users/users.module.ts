f7622ae62a75f65a1feeea60773ef80b04ed66ff
BlackBeard007 <princesagor159@gmail.com>
2025-11-06 23:22:35 +0600

    Refactor user module and service to integrate Mongoose for user schema management

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { Module } from '@nestjs/common'; 
+import { MongooseModule } from '@nestjs/mongoose'; 
+import { User, UserSchema } from '../database/schemas/user.schema'; 
+import { UsersService } from './users.service'; 
+import { UsersController } from './users.controller'; 
 
-@Module({
-  providers: [UsersService],
-  controllers: [UsersController]
-})
+@Module({ 
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])], 
+  providers: [UsersService], 
+  controllers: [UsersController], 
+  exports: [UsersService], 
+}) 
+
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'; 
+import { InjectModel } from '@nestjs/mongoose'; 
+import { Model } from 'mongoose'; 
+import { User, UserDocument } from '../database/schemas/user.schema'; 
 
-@Injectable()
-export class UsersService {}
+@Injectable() 
+export class UsersService { 
+
+    constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {} 
+
+    async findByEmail(email: string) { 
+        return this.userModel.findOne({ email }); 
+    }
+
+    async createUser(userData: Partial<User>) { 
+        const user = new this.userModel(userData); 
+        return user.save(); 
+    }
+     
+    async addProvider(user: UserDocument, provider: string, oauthId?: string) { 
+        if (!user.providers.includes(provider)) user.providers.push(provider); 
+        if (oauthId) user.oauthIds[provider] = oauthId; 
+        return user.save(); 
+    }
+     
+    async findById(id: string) { 
+        return this.userModel.findById(id); 
+    } 
+}
